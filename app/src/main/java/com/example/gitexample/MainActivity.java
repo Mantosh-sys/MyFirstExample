57f2492557e10f2a77223c55cc2c116578ae4cbe
Mantosh-sys <singhmantosh964@gmail,com>
2021-06-01 17:47:38 +0530

    Initial Comment1

--- app/src/main/java/com/example/gitexample/MainActivity.java
+++ app/src/main/java/com/example/gitexample/MainActivity.java
@@ -3,11 +3,13 @@ package com.example.gitexample;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toast.makeText(getApplicationContext(),"hi",Toast.LENGTH_LONG).show();
     }
 }
